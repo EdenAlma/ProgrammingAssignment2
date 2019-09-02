ce9340411eba4c486d86d95034231971b3d9d359
EdenAlma <eden.almakias@gmail.com>
2019-09-01 21:53:51 -0400

    corrected a few issues: the set() function added message when "getting matrix from cache"

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,52 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##This function mimics a constructor from a OOP perspective holding/tracking 2 variables (iMatrix and x)
+##and 4 functions which are used to access and set the two variable (functions are returned in list)
 makeCacheMatrix <- function(x = matrix()) {
+  
+  iMatrix <- NULL     #inverse matrix var
+  
+  get <- function() x 
+  
+  set <- function(y) {
+    x <<- y
+    iMatrix <<- NULL
+    } 
+  #basically sets the input matrix by refrencing using <<- operator
+  
+  getInv <- function() iMatrix #get iMatrix
+  setInv <- function(inpt) iMatrix <<- inpt #set iMatrix by refrencing upper scope iMatrix
+  
+  z<-list(get = get, set = set, setInv = setInv, getInv = getInv) #create list to be returned
+  z
 
 }
 
 
 ## Write a short comment describing this function
+##This functiom will be used to access/create the "cacheMatrix" objects inverse matrix vaiable
+##x is a cacheMatrix list/object and this function will use the four functions to access and set
+##the iMatrix variable 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  iM <- x$getInv() ##get inverted matrix value 
+   
+  if (is.null(iM)){ ##if not set >> calculate and cash
+    
+    m <-  x$get() ##get matrix
+    i <- solve(m, ...) ##calculate inverse matrix
+    x$setInv(i) ##cache the calculated inverse matrix
+    return(i)
+  }
+  
+  message("Getting matrix from cache")
+  r <- x$getInv() ##get cached matrix
+  r  ##return inverse matrix
+  
+  
 }
+
+
